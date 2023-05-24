643df58efe19c1f96cc6ba9bfa3a2dcb794a06ad
Mara Green <maragreen42@gmail.com>
2023-05-23 21:27:21 -0700

    working on the game_over global variable

--- blackjack.py
+++ blackjack.py
@@ -3,13 +3,15 @@ import random
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 end_game = False
 
+print(logo)
+print("Let's play Blackjack!")
 
 def deal_card():
     user_hand = random.sample(cards, 2)
     dealer_hand = random.sample(cards, 2)
     print(f"""
-    Your hand: {user_hand}
-    Dealer first card: {dealer_hand[0]} """)
+Your hand: {user_hand}
+Dealer hand: [{dealer_hand[0]}, ?] """)
     hands = [user_hand, dealer_hand]
     return hands
     
@@ -21,8 +23,8 @@ def calculate_score(hands):
         print(f"You have a Blackjack! You win! 🃏")
         end_game = True
     elif dealer_score == 21:
-        print("The dealer has a blackjack. You lose ☹️")
         end_game = True
+        print("The dealer has a blackjack. You lose ☹️")
     scores = [user_score, dealer_score]
     return scores
     
@@ -31,19 +33,21 @@ def check_who_wins(scores):
     if scores[0] > 21 and scores[1] < 21:
         print(f"Bust! Your score is {scores[0]},You lose")
         end_game = True
+        
     elif scores[1] > 21 and scores[0] < 21:
         print(f"You win! Dealer score was {scores[1]} ")
         end_game = True
+        
 
-def draw_a_card(scores):
-    draw = input("Would you like another card? y or n: ")
+def draw_a_card(scores, hands):
+    draw = input(f"Your score is {scores[0]}, Would you like another card? y or n: ")
+    another_card = int(random.choice(cards))
     if draw == "y":
-        another_card = random.randint(cards)
-        scores[0] = scores[0].append(another_card)
-        calculate_score(scores)
-    else:
-        scores[1] = scores[1].append(another_card)
-        calculate_score(scores)
+        hands[0].append(another_card)
+        calculate_score(hands)
+    elif scores[1] < 17: 
+        hands[1].append(another_card)
+        calculate_score(hands)
 
         
     
@@ -63,7 +67,7 @@ def run_game():
             if end_game == True:
                 print("Game Over. Thank you for playing")
             else:
-                draw_a_card(scores)
+                draw_a_card(scores, hands)
 
 
 run_game()
